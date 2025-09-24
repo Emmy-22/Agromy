7bafacf69038c700a6aabdeaa53a3d6d5b29a48f
Akintobi Abibat Olamide <akintobiabibat@yahoo.com>
2025-09-24 11:44:02 +0000

    added little styling to the sections

--- src/Pages/MarketPlace.jsx
+++ src/Pages/MarketPlace.jsx
@@ -6,15 +6,15 @@ const MarketPlace = () => {
     <section id="MarketPlace" className="w-full bg-white py-20 px-4 sm:px-6 lg:px-8 text-center">
       {/* Section 1 */}
       <div className="py-6">
-        <h2 className="text-5xl lg:text-7xl font-extrabold text-[#0F4D0F] mb-10 sm:p-0">
+        <h2 className="text-5xl lg:text-7xl font-extrabold text-[#0F4D0F] mb-10 sm:p-0 text-center">
           What You Get With Agromy
         </h2>
       </div>
 
       {/* Cards Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 max-w-7xl mx-auto px-1 lg:px-4 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 max-w-7xl mx-auto px-1 lg:px-4 cursor-pointer">
         {/* Farmers */}
-        <div className="bg-gradient-to-l from-[#008000] to-[#0F4D0F] text-white rounded-2xl p-10 sm:p-12 lg:p-14 shadow-md">
+        <div className="bg-gradient-to-l from-[#008000] to-[#0F4D0F] text-white rounded-2xl p-10 sm:p-12 lg:p-14 shadow-md hover:scale-[1.02] transition-transform duration-300">
           <div className="w-60 h-14 mx-auto mb-6  bg-white text-black flex items-center justify-center rounded-full font-bold text-lg">
             <LockKeyhole size={26} />
           </div>
@@ -25,7 +25,7 @@ const MarketPlace = () => {
         </div>
 
         {/* Buyers */}
-        <div className="bg-gradient-to-tr from-[#0F4D0F] to-[#008000] text-white rounded-2xl p-10 sm:p-12 lg:p-14 shadow-md">
+        <div className="bg-gradient-to-tr from-[#0F4D0F] to-[#008000] text-white rounded-2xl p-10 sm:p-12 lg:p-14 shadow-md hover:scale-[1.02] transition-transform duration-300">
           <div className="w-60 h-14 mx-auto mb-6 bg-white text-black flex items-center justify-center rounded-full font-bold text-lg">
             <LockKeyhole size={26} />
           </div>
@@ -36,7 +36,7 @@ const MarketPlace = () => {
         </div>
 
         {/* Distributors */}
-        <div className="bg-gradient-to-l from-[#008000] to-[#0F4D0F] text-white rounded-2xl p-10 sm:p-12 lg:p-14 shadow-md">
+        <div className="bg-gradient-to-l from-[#008000] to-[#0F4D0F] text-white rounded-2xl p-10 sm:p-12 lg:p-14 shadow-md hover:scale-[1.02] transition-transform duration-300">
           <div className="w-60 h-14 mx-auto mb-6 bg-white text-black flex items-center justify-center rounded-full font-bold text-lg">
             <LockKeyhole size={26} />
           </div>
@@ -47,7 +47,7 @@ const MarketPlace = () => {
         </div>
 
         {/* Job Seekers */}
-        <div className="bg-gradient-to-tr from-[#0F4D0F] to-[#008000] text-white rounded-2xl p-10 sm:p-12 lg:p-14 shadow-md">
+        <div className="bg-gradient-to-tr from-[#0F4D0F] to-[#008000] text-white rounded-2xl p-10 sm:p-12 lg:p-14 shadow-md hover:scale-[1.02] transition-transform duration-300">
           <div className="w-60 h-14 mx-auto mb-6 bg-white text-black flex items-center justify-center rounded-full font-bold text-lg">
             <LockKeyhole size={26} />
           </div>
